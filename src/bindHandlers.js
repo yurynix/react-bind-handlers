262bda57630d9e0bd04581434140a110adb9fc34
Filip Fiolka <fiolkaf@gmail.com>
2016-05-26 10:13:11 +0200

    code review - rename handlerNames => methodNames

--- src/bindHandlers.js
+++ src/bindHandlers.js
@@ -1,12 +1,12 @@
 export function bind(regexp) {
     return target => {
         const keys = Object.getOwnPropertyNames(target.prototype);
-        const handlerNames = keys.filter(key => key.match(regexp));
+        const methodNames = keys.filter(key => key.match(regexp));
 
         class Component extends target {
             constructor(props) {
                 super(props);
-                handlerNames.forEach(handlerName => this[handlerName] = this[handlerName].bind(this));
+                methodNames.forEach(handlerName => this[handlerName] = this[handlerName].bind(this));
             }
         }
 
